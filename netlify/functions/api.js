6bde4fdd7d266d9a128373e0eeaba6af8c2fafed
suxia0 <150435708+suxia0@users.noreply.github.com>
2025-06-07 16:28:03 +0800

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,13 +15,13 @@ const responseData = {
     "news": "⚠️解析失败请重试(疯果科技)",
     "low7": "⚠️该视频清晰度低于720,不提供下载(疯果科技)",
     "data": {
-        "version": "公众号:疯果科技(1元)2689",
+        "version": "公众号:疯果科技(1元)26899",
         "updata": "点击按钮更新⬇️(公众号:疯果科技)",
         "updat": "点击按钮更新⬇️⬇️⬇️(作者:疯果科技)",
         "newmsg": "公众号：疯果科技(1元)2689",
         "vcode": "419",
         "title": "⚠️盗用全家",
-        "url": "https://www.icloud.com/shortcuts/efc09d3bd0d0492c917a69e987fb9681"
+        "url": "https://www.icloud.com/shortcuts/588f30c2f7574d68bf711948f50a0bbb"
     }
 };
 
